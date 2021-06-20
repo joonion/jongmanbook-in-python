24186e5be8342232ba90f8015aae5175ac692a0b
joonion <joonion@gmail.com>
2021-06-20 12:05:41 +0900

    JOSEPHUS

--- JOSEPHUS.py
+++ JOSEPHUS.py
@@ -1,9 +1,13 @@
-T = int(input())
-for _ in range(T):
-    n, k = map(int, input().split())
+def solve(n, k):
     d, q = 0, list(range(1, n + 1))
     while n > 2:
         q.pop(d)
         n -= 1
         d = (d + k - 1) % n
-    print(q[0], q[1])
+    return q[0], q[1]
+
+T = int(input())
+for _ in range(T):
+    n, k = map(int, input().split())
+    S = solve(n, k)
+    print(S[0], S[1])
